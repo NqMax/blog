553e61474d6e8fdb73d02eec18f69b96ac4ed49c
Max <111196993+NqMax@users.noreply.github.com>
2024-04-12 00:36:10 -0600

    Implemented grid gallery component

---
+++ src/components/gallery.tsx
@@ -0,0 +1,11 @@
+export function Gallery() {
+  return (
+    <div className="grid h-full grid-cols-3 grid-rows-3 gap-3">
+      <div className="rounded border border-main bg-[url(/home/grid/grid-1.svg)] bg-cover" />
+      <div className="row-span-2 rounded border border-main bg-[url(/home/grid/grid-2.svg)] bg-cover" />
+      <div className="row-span-3 rounded border border-main bg-[url(/home/grid/grid-3.svg)] bg-cover" />
+      <div className="row-span-2 rounded  border border-main bg-[url(/home/grid/grid-4.svg)] bg-cover" />
+      <div className="rounded border border-main bg-[url(/home/grid/grid-5.svg)] bg-cover" />
+    </div>
+  );
+}
