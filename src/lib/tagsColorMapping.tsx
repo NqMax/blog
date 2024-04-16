8491cf9dabf0de331383fe8c10f778165f98b06e
Max <111196993+NqMax@users.noreply.github.com>
2024-04-16 00:28:49 -0600

    Added object to map tags to hover color classes

---
+++ src/lib/tagsColorMapping.tsx
@@ -0,0 +1,9 @@
+export const tagsColorMapping = {
+  TypeScript: "hover:text-blue-500",
+  JavaScript: "hover:text-yellow-400",
+  CSS: "hover:text-blue-600",
+  React: "hover:text-sky-500",
+  Tailwind: "hover:text-sky-400",
+  "Node.js": "hover:text-green-500",
+  Rust: "hover:text-red-500",
+};
