d9cac601623c3943dd565bdbf94f6afe6d75aefc
Max <111196993+NqMax@users.noreply.github.com>
2024-04-21 14:21:51 -0600

    Moved intro into its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,37 +2,13 @@
 import { Gallery } from "@/components/gallery";
 import { Socials } from "@/components/socials";
 import { LatestArticles } from "@/components/latestArticles";
+import { Intro } from "@/components/intro";
 
 export default function Home() {
   return (
     <div className="container flex min-h-screen gap-x-10">
       <main className="sticky top-0 flex max-h-screen w-1/2 flex-col gap-y-4 py-24">
-        <div className="mb-8">
-          <p className="font-mono text-lg text-main">Hi, my name is Max</p>
-          <h1 className="font-mono text-4xl font-bold text-hl [text-shadow:#fff_0px_0px_0px]">
-            Welcome to my blog.
-          </h1>
-        </div>
-        <p>
-          My journey into the{" "}
-          <span className="font-medium text-hl">world of coding</span> began
-          with a fascination for{" "}
-          <span className="font-medium text-hl">problem-solving</span> and a
-          desire to create impactful solutions through{" "}
-          <span className="font-medium text-hl">technology</span>. I thrive on
-          challenges, constantly exploring new frameworks and languages to
-          expand my skill set.
-        </p>
-        <p>
-          With a solid foundation in{" "}
-          <span className="font-medium text-hl">front-end and back-end</span>{" "}
-          development, I am committed to honing my abilities to craft seamless,
-          user-centric experiences and{" "}
-          <span className="font-medium text-hl">
-            robust, scalable applications
-          </span>{" "}
-          that impact our world.
-        </p>
+        <Intro />
         <Gallery />
         <div className="flex gap-x-4">
           <Socials />
@@ -40,7 +16,9 @@ export default function Home() {
         </div>
       </main>
       <div className="flex w-1/2 flex-col py-24">
-        <h2 className="font-mono text-lg text-main"><span className="underline underline-offset-2">home</span> blog</h2>
+        <h2 className="font-mono text-lg text-main">
+          <span className="underline underline-offset-2">home</span> blog
+        </h2>
         <LatestArticles />
       </div>
     </div>
---
+++ src/components/intro.tsx
@@ -0,0 +1,32 @@
+export function Intro() {
+  return (
+    <>
+      <div className="mb-2">
+        <p className="font-mono text-lg text-main">Hi, my name is Max</p>
+        <h1 className="font-mono text-4xl font-bold text-hl [text-shadow:#fff_0px_0px_0px]">
+          Welcome to my blog.
+        </h1>
+      </div>
+      <p>
+        My journey into the{" "}
+        <span className="font-medium text-hl">world of coding</span> began with
+        a fascination for{" "}
+        <span className="font-medium text-hl">problem-solving</span> and a
+        desire to create impactful solutions through{" "}
+        <span className="font-medium text-hl">technology</span>. I thrive on
+        challenges, constantly exploring new frameworks and languages to expand
+        my skill set.
+      </p>
+      <p>
+        With a solid foundation in{" "}
+        <span className="font-medium text-hl">front-end and back-end</span>{" "}
+        development, I am committed to honing my abilities to craft seamless,
+        user-centric experiences and{" "}
+        <span className="font-medium text-hl">
+          robust, scalable applications
+        </span>{" "}
+        that impact our world.
+      </p>
+    </>
+  );
+}
