64b72b14096f88154c977bc827257e20bb946709
Max <111196993+NqMax@users.noreply.github.com>
2024-04-21 14:22:06 -0600

    UI Changes

--- src/components/articleCard.tsx
+++ src/components/articleCard.tsx
@@ -19,16 +19,16 @@ export function ArticleCard({
   return (
     <article className="flex flex-col gap-y-2 border-b border-dashed py-4 first:pb-4">
       <Link href={url} className="group flex flex-col gap-y-2">
-        <div className="flex items-center gap-x-2 text-nowrap">
-          <h3 className="text-lg font-medium text-hl underline decoration-transparent transition group-hover:decoration-current">
+        <div className="flex items-center gap-x-2">
+          <h3 className="text-lg font-medium text-hl truncate underline decoration-transparent transition group-hover:decoration-current">
             {title}
           </h3>
-          <span className="w-full border-b border-main" />
-          <time className="text-sm text-neutral-400 transition group-hover:text-foreground">
+          <span className="grow border-b min-w-10 border-main" />
+          <time className="text-sm text-neutral-500 transition group-hover:text-foreground">
             {date}
           </time>
         </div>
-        <p className="line-clamp-3 text-neutral-400 transition group-hover:text-foreground">
+        <p className="line-clamp-3 text-neutral-500 transition group-hover:text-foreground">
           {introduction}
         </p>
       </Link>
